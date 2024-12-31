0daeda93b80f47026d44b88233719d98e26440a7
Jean Sacren <sakiwit@gmail.com>
2024-12-30 20:09:42 -0700

    Add helper to filter out users
    Wrap up the filter logic in a helper that is configurable for changes
    without hard coding 'Test User'.
    
    Add User type declaration in the fn parameter and explicitly ensure that
    the $user variable is treated as an instance of the User model.

--- database/seeders/PostSeeder.php
+++ database/seeders/PostSeeder.php
@@ -9,12 +9,19 @@ use Illuminate\Database\Seeder;
 class PostSeeder extends Seeder
 {
     /**
+     * Filter users helper.
+     */
+    public function filterUsers(User $user): bool
+    {
+        return $user->name !== 'Test User';
+    }
+    /**
      * Run the database seeds.
      */
     public function run(): void
     {
         $users = User::all()->filter(
-            fn ($user) => $user->name !== 'Test User'
+            fn (User $user) => $this->filterUsers($user)
         );
 
         foreach ($users as $user) {
